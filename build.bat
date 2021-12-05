4b786d805dba3fe531b06aeafecc5c8667fbbc8b
Joshua Willis <61606486+parsrnet@users.noreply.github.com>
2021-12-05 12:29:47 -0800

    Updated build.bat
    Updated build.bat to handle building, container spinup, and attaching

--- build.bat
+++ build.bat
@@ -1,10 +1,18 @@
-:: Build and create new image from .dockerfile. If no dockerfile path is specified, then uses default behavior (PATH/dockerfile). ::
+:: Created by Joshua Willis, adapted for Windows from existing code by Manoj Joshi ::
 @ECHO OFF
-:: Dir to dockerfile
-SET dockerfile="Dockerfile"
-:: Can be any name.
-SET imageName="dockerdev"
-:: Any directory
-SET contextPath="."
 
-docker build -f %dockerfile% -t %imageName% %contextPath% || ECHO Operation failed. See above for details. & PAUSE
+:: Build the image from the dockerfile ::
+SET dockerfile=Dockerfile
+SET imageName=dockerdev
+SET contextPath=.
+
+docker build -f %dockerfile% -t %imageName% %contextPath% || ECHO An error occurred which interupted the build operation. See above for details && PAUSE
+
+:: Spin up a new container ::
+
+:: Create docker container using docker run command and set contents to variable %container%, which we will then use to attach to container process.
+FOR /f "USEBACKQ" %%a IN (`docker run -itd %imageName% bash`) DO SET container=%%a
+ECHO Attempting to connect to container id = %container%
+
+:: Attach to new container ::
+START CMD /c docker exec -it %container% bash
